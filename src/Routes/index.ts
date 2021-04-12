83be45e196440f40c91294d47ca6be32767d2ef3
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-12 18:15:32 -0300

    removido scripts de teste

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -6,14 +6,11 @@ import authenticate from "../Controllers/authentication"
 import addUser from "../Controllers/addUser/addUser"
 import addLocal from "../Controllers/addLocal"
 import getLocalsByMap from "../Controllers/getLocalsByMap"
-import teste from "../Controllers/teste"
 
 const app = Express()
 app.use(json())
 
 export default function routes() {
-
-    app.get("/teste", authMiddleware, teste)
     app.post("/addLocal", authMiddleware, addLocal)
     app.get("/getLocalsByMap", authMiddleware, getLocalsByMap)
     app.post("/user", authMiddleware, addUser)
