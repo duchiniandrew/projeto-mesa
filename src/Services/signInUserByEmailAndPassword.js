131aaa6ea37a0a088497eaa8b657d1556cc8452e
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-13 18:37:38 -0300

    removido JS gerados

--- src/Services/signInUserByEmailAndPassword.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var typedi_1 = require("typedi");
-function signInUserByEmailAndPassword(email, password) {
-    return typedi_1.Container.get("UserTable").findOne({ email: email, password: password });
-}
-exports["default"] = signInUserByEmailAndPassword;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DB_1 = require("./DB");
-var dotenv_1 = require("dotenv");
-dotenv_1.config();
-DB_1["default"]();
