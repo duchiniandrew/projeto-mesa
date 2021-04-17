542ab5888594a5e93e2a669890dd5d8b011ba122
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-17 17:37:28 -0300

    correção

--- app/Services/changeUserEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typedi_1 = require("typedi");
-async function changeUserPasswordService(newEmail, userId) {
-    await typedi_1.Container.get("UserTable").save({ id: userId, email: newEmail });
-    return { message: "User email successfully updated." };
-}
-exports.default = changeUserPasswordService;
--- app/Services/logout.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typedi_1 = require("typedi");
-async function logout(token) {
-    return typedi_1.Container.get("TokenBlackListTable").save({ token: token });
-}
-exports.default = logout;
