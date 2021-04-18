efeaf31133e7910c1774d5b7b9ce2dd026889905
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-18 13:32:24 -0300

    corrigido BUG

--- src/Controllers/addUser/validation.ts
+++ src/Controllers/addUser/validation.ts
@@ -14,7 +14,27 @@ export default async function validation(req: Request) {
     if (password === "") {
         throw new EmptyPasswordError()
     }
-    if (password.length < 8 && /[^A-Za-z0-9]+/.test(password)) {
+    ///[^A-Za-z0-9]+/.test(password)
+    //REGEx here doesn't work
+    if (password.length < 8 || !testPassword(password)) {
         throw new FormatPasswordError()
     }
+}
+function testPassword(password: string) {
+    let hasNumber = false
+    let hasLetters = false
+
+    //TODO find a better way to check password format
+    for (let i = 0; i < password.length; i++) {
+        if (/[a-zA-Z]/.test(password[i])) {
+            hasLetters = true
+        }
+        if (/\d+/g.test(password[i])) {
+            hasNumber = true
+        }
+        if (hasLetters && hasNumber) {
+            return true
+        }
+    }
+    return false
 }
