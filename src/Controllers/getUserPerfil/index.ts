cad00a922197558d578d1e41390cdcbbaaccc808
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-18 14:07:28 -0300

    removido validação denecessária

--- src/Controllers/getUserPerfil/index.ts
+++ src/Controllers/getUserPerfil/index.ts
@@ -1,18 +1,11 @@
 import { Request, Response } from "express";
 
-import validation from "./validation"
 import getUserPerfilService from "../../Services/getUserPerfil"
 import getEmailFromToken from "../../Services/getEmailFromToken"
 
 export default async function getUserPerfil(req: Request, res: Response) {
     try {
-        validation(req)
-        try {
-            res.status(200).json(await getUserPerfilService(getEmailFromToken(req)))
-        }
-        catch (error) {
-            res.status(error.code).json({ message: error.message })
-        }
+        res.status(200).json(await getUserPerfilService(getEmailFromToken(req)))
     }
     catch (error) {
         res.status(error.code).json({ message: error.message })
