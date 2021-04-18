b694ff31133b06c692a03c22b8173e7413e466a3
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-04-18 14:04:57 -0300

    removido console.log

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -8,14 +8,11 @@ import { LocalAnalysis } from "./Entities/LocalAnalysis"
 import { TokenBlackList } from "./Entities/TokenBlackList"
 
 export default function db() {
-
     createConnection().then(connection => {
         Container.set("UserTable", connection.getRepository(User))
         Container.set("LocalTable", connection.getRepository(Local))
         Container.set("LocalAnalysisTable", connection.getRepository(LocalAnalysis))
         Container.set("TokenBlackListTable", connection.getRepository(TokenBlackList))
         Routes()
-    }).catch(e => {
-        console.log(e)
     })
 }
--- src/Services/getUserPerfil.ts
+++ src/Services/getUserPerfil.ts
@@ -4,7 +4,6 @@ import { Repository } from "typeorm"
 import { User } from "../DB/Entities/User"
 
 export default function getUserPerfil(email: string) {
-    console.log(email)
     return Container.get<Repository<User>>("UserTable").createQueryBuilder("user")
         .select(['user.email', 'user.name',])
         .where("user.email = :email", { email: email }).getOne()
